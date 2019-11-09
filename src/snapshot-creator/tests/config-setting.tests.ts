d484f4b9f04fa3622d8b3bccef405e80cbc37ab0
Eric Wang <wjun0912@gmail.com>
2019-11-09 19:08:40 +1100

    Remove unused test (#10)

--- src/snapshot-creator/tests/config-setting.tests.ts
+++ src/snapshot-creator/tests/config-setting.tests.ts
@@ -9,26 +9,6 @@ describe('when set the config', () => {
     },
     parser: '@typescript-eslint/parser',
   });
-  const code = 'const a = 1';
-  const options = ['always'];
-  const config = {};
-
-  it.skip('should combine the config to worker', () => {
-    snapshotCreator
-      .mark(code)
-      .onRule('semi', semiRule)
-      .withOptions(options)
-      .overrideConfig(config);
-
-    expect((snapshotCreator as any).config).toEqual({
-      parserOptions: {
-        ecmaVersion: 2017,
-        sourceType: 'module',
-      },
-      parser: '@typescript-eslint/parser',
-      options: ['always'],
-    });
-  });
 
   it('throws if rule is not provided', () => {
     const fn = () => snapshotCreator.mark('').render();
