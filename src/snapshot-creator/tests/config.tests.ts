869e9e23558693029770ad6f91f8a1d1c1319e1d
Eric Wang <wjun0912@gmail.com>
2019-11-10 12:59:44 +1100

    refactor filename implementation (#15)

--- src/snapshot-creator/tests/config.tests.ts
+++ src/snapshot-creator/tests/config.tests.ts
@@ -2,27 +2,64 @@ import { SnapshotCreator } from "../snapshot-creator";
 import { createAssertConfigRule } from "../../utils/testing-rules/assert-config";
 
 describe('when providing configs', () => {
-  const snapshotCreator = new SnapshotCreator({
-    parser: '@typescript-eslint/parser',
-  });
+  const parserOptions = {
+    ecmaVersion: 2017 as 2017,
+    sourceType: 'module' as 'module',
+  };
+  const code = '';
+  const ruleName = 'ruleName';
+
+  let snapshotCreator: SnapshotCreator;
+
+  beforeEach(() => {
+    snapshotCreator = new SnapshotCreator({
+      parser: '@typescript-eslint/parser',
+      parserOptions,
+    });
+  })
 
   it('can parse the filename correctly', () => {
     const wantFileName = 'want.filename.tsx';
     let gotFilename: string = '';
     const rule = createAssertConfigRule(context => gotFilename = context.getFilename());
     snapshotCreator
-      .mark({ code: '', ruleName: 'assert-filename', rule })
+      .mark({ code, ruleName , rule })
       .withFileName(wantFileName)
       .render();
     expect(gotFilename).toEqual(wantFileName);
   });
 
-  it('get empty string if the filename is not provided', () => {
+  it('get <intpu> if the filename is not provided', () => {
     let gotFilename: string = '';
     const rule = createAssertConfigRule(context => gotFilename = context.getFilename());
     snapshotCreator
-      .mark({ code: '', ruleName: 'assert-filename', rule })
+      .mark({ code, ruleName , rule })
       .render();
-    expect(gotFilename).toEqual('');
+    expect(gotFilename).toEqual('<input>');
+  });
+
+  it('can add the settings', () => {
+    let gotSettings: any = {};
+    const wantSetting = { a: 1, b: 2, c: 3 };
+    const rule = createAssertConfigRule(context => gotSettings = context.settings);
+    snapshotCreator
+      .mark({ code, ruleName , rule })
+      .overrideConfig({ settings: wantSetting })
+      .render();
+    expect(gotSettings).toEqual(wantSetting)
+  });
+
+  it('overrides ecmaVersion in parserOptions', () => {
+    let gotParserOptions: any = {};
+    const wantParserOptions = {
+      ...parserOptions,
+      ecmaVersion: 6,
+    };
+    const rule = createAssertConfigRule(context => gotParserOptions = context.parserOptions);
+    snapshotCreator
+      .mark({ code: '', ruleName: 'assert-parser-options', rule })
+      .overrideConfig({ parserOptions: { ecmaVersion: 6 } })
+      .render();
+    expect(gotParserOptions).toEqual(expect.objectContaining(wantParserOptions))
   });
 });
