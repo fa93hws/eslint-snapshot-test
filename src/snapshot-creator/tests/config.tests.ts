3ddae59cd7a1601b2c02d536288916126dca36bf
eric <eric@canva.com>
2019-11-10 12:19:39 +1100

    Allow providing filename

---
+++ src/snapshot-creator/tests/config.tests.ts
@@ -0,0 +1,23 @@
+import { SnapshotCreator } from "../snapshot-creator";
+import { createAssertConfigRule } from "../../utils/testing-rules/assert-config";
+
+describe('when providing configs', () => {
+  const snapshotCreator = new SnapshotCreator({
+    parserOptions: {
+      ecmaVersion: 2017,
+      sourceType: 'module',
+    },
+    parser: '@typescript-eslint/parser',
+  });
+
+  it('can parse the filename correctly', () => {
+    const wantFileName = 'want.filename.tsx';
+    let gotFilename: string = '';
+    const rule = createAssertConfigRule(context => gotFilename = context.getFilename());
+    snapshotCreator
+      .mark({ code: '', ruleName: 'assert-filename', rule })
+      .withFileName(wantFileName)
+      .render();
+    expect(gotFilename).toEqual(wantFileName);
+  });
+});
---
+++ src/utils/testing-rules/assert-config.ts
@@ -0,0 +1,29 @@
+import { RuleContext } from '@typescript-eslint/experimental-utils/dist/ts-eslint';
+import { createRule } from './create-rule';
+
+type MessageIds = 'config';
+type Option = [];
+export const name = 'assert-config';
+const messages: Record<MessageIds, string> = {
+  config: "no error msg for this rule",
+};
+
+export function createAssertConfigRule(callback: (context: RuleContext<'config', []>) => void) {
+  return createRule<Option, MessageIds>({
+    name,
+    meta: {
+      type: 'suggestion',
+      schema: [],
+      messages,
+      docs: {
+        description: 'Must have comment in message file',
+        category: 'Best Practices',
+        recommended: 'error',
+      },
+    },
+    defaultOptions: [],
+    create: (context) => ({
+      Program: () => callback(context),
+    }),
+  });
+}
