503a95001a33b1df060c4bbe79e8af4e681cab73
eric <eric@canva.com>
2019-11-24 14:52:08 +1100

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,7 @@ module.exports = {
     'airbnb-base',
     'eslint-config-prettier',
   ],
-  plugins: [
-    '@typescript-eslint',
-    'import',
-    'jest',
-  ],
+  plugins: ['@typescript-eslint', 'import', 'jest'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
@@ -29,9 +25,12 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: ['tests/**/*.ts',],
-    }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['tests/**/*.ts'],
+      },
+    ],
     'import/no-extraneous-dependencies': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     'no-underscore-dangle': 'off',
@@ -39,5 +38,5 @@ module.exports = {
     'global-require': 'off',
     'no-useless-constructor': 'off',
     'no-empty-function': 'off',
-  }
-}
+  },
+};
