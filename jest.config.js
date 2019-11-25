8ef6450d36768287e157517cf5d0fd9aceefc126
eric <eric@canva.com>
2019-11-25 22:41:43 +1100

    .

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   roots: ['<rootDir>'],
   preset: 'ts-jest',
   testMatch: ['**/tests/**/*.tests.ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**', '!src/index.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!**/node_modules/**'],
   coverageDirectory: 'coverage',
 };
